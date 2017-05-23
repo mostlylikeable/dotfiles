786ecbec423fcb58eaa55f7a8bb4a2176be0d3ef
boggebe <ben.boggess@gmail.com>
2017-05-23 09:30:17 -0500

    initial commit -- incomplete

---
+++ sh/env.sh
@@ -0,0 +1,31 @@
+
+# default shell
+export SHELL=/bin/zsh
+
+# default editor
+export EDITOR=vim
+
+# use english + utf-8
+export LC_ALL="en_US.UTF-8"
+
+# local-auth: 57ed4cdf-9aee-4c84-841f-5e1e07de5bfa
+
+# groovy/grails
+export GVM_HOME=$HOME/.gvm
+export GRAILS_HOME=$GVM_HOME/grails/current
+export GROOVY_HOME=$GVM_HOME/groovy/current
+
+# java
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
+# bash completion
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+  . `brew --prefix`/etc/bash_completion
+fi
+
+alias srcpro='. ~/.zshrc'
+
+#THIS MUST BE AT THE END OF THE FILE FOR GVM TO WORK!!!
+#[[ -s "$HOME/.gvm/bin/gvm-init.sh" && -z $(which gvm-init.sh | grep '/gvm-init.sh') ]] && source "$HOME/.gvm/bin/gvm-init.sh"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" && -z $(which sdkman-init.sh | grep '/sdkman-init.sh') ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+#source "$HOME/.sdkman/bin/sdkman-init.sh"
---
+++ sh/functions.sh
@@ -0,0 +1,88 @@
+
+# set java home to java 8 jvm
+function j8() {
+  # outputs error if no java 8 jvm installed
+  export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+  java -version
+}
+
+# set java home to java 7 jvm
+function j7() {
+  # outputs error if no java 7 jvm installed
+  export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
+  java -version
+}
+
+function jver() {
+  version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+  echo $version
+}
+
+# exec gradle wrapper in current or parents
+function gradle() {
+  gw="$(upfind gradlew)"
+  if [ -z "$gw" ]; then
+    echo "Gradle wrapper not found."
+  else
+    $gw $@
+  fi
+}
+
+# find $1 in current dir, or parents
+function upfind() {
+  dir=`pwd`
+  while [ "$dir" != "/" ]; do
+    p=`find "$dir" -maxdepth 1 -name $1`
+    if [ ! -z $p ]; then
+      echo "$p"
+      return
+    fi
+    dir=`dirname "$dir"`
+  done
+}
+
+function mcd() {
+  mkdir -p "$1" && cd "$1"
+}
+
+# find shorthand
+# https://github.com/nicksp/dotfiles/blob/master/shell/shell_functions
+function f() {
+  find . -name "$1" 2>&1 | grep -v 'Permission denied'
+}
+
+# List all files, long format, colorized, permissions in octal
+function la() {
+   ls -l  "$@" | awk '
+    {
+      k=0;
+      for (i=0;i<=8;i++)
+        k+=((substr($1,i+2,1)~/[rwx]/) *2^(8-i));
+      if (k)
+        printf("%0o ",k);
+      printf(" %9s  %3s %2s %5s  %6s  %s %s %s\n", $3, $6, $7, $8, $5, $9,$10, $11);
+    }'
+}
+
+# https://unix.stackexchange.com/questions/74185/how-can-i-prevent-grep-from-showing-up-in-ps-results
+function pids() {
+  # ps aux | grep "[o]auth" | awk '{print $2}'
+  ps aux | grep "[${1:0:1}]${1:1}" | awk '{print $2}'
+}
+
+# override rm to send to trash (timestamped) instead
+function rm () {
+    local path
+    for path in "$@"; do
+        # ignore any arguments
+        if [[ "$path" = -* ]]; then :
+        else
+            local dst=${path##*/}
+            # append the time if necessary
+            while [ -e ~/.Trash/"$dst" ]; do
+                dst="$dst "$(date +%H-%M-%S)
+            done
+            mv "$path" ~/.Trash/"$dst"
+        fi
+    done
+}
